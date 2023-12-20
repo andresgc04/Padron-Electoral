f2f304f527ed62275c48241705e1ec2d5991f726
andresgc04 <andresgc1997@outlook.com>
2023-12-20 00:46:55 -0400

    Creando Variables De Entorno y Unificacion De Respuestas

--- src/modulos/personas/rutas.js
+++ src/modulos/personas/rutas.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
+const respuesta = require("../../red/respuestas");
+
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.send("Personas Ok");
+  respuesta.success(req, res, "Todo Ok En Clientes", 200);
 });
 
 module.exports = router;
